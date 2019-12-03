d46a7031f7103a0b590355769d46be2cdbf555d3
tianfeng <tianfeng3@xiaomi.com>
2019-12-03 18:05:07 +0800

    sharding-jdbc初步使用

--- src/main/java/com/tf/vegout/sharding/ShardingApplication.java
+++ src/main/java/com/tf/vegout/sharding/ShardingApplication.java
@@ -1,5 +1,6 @@
 package com.tf.vegout.sharding;
 
+import com.sun.xml.internal.ws.api.databinding.DatabindingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
